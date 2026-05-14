5afd47edd1cd8bf00f32eeaf22b614d843fbf63e
MAURIGG <mauricio.galarza2004@gmail.com>
2026-05-14 16:26:04 -0300

    Delete db directory

--- db/modelos/usuario.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-class Usuario (BaseModel):
-    id: str | None = None
-    usuario: str
-    email: str
--- db/schemas/usuario.py
+++
@@ -1,8 +0,0 @@
-def usuario_esquema(usuario)-> dict :
-    return {"id":str(usuario["_id"]),
-            "usuario":usuario["usuario"],
-            "email":usuario["email"]}
-
-
-def usuarios_esquema(usuarios) -> list:
-    return [usuario_esquema(usuario) for usuario in usuarios] 
